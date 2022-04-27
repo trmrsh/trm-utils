04197f4a4868f2447e0b2e68c7f2e6c444d56997
Tom Marsh <t.r.marsh@warwick.ac.uk>
2022-04-27 18:05:14 +0100

    setup.py -- tweaks to get going

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,50 +7,46 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='trm.utils',
-      version='0.0.1',
+setup(
+    name='trm.utils',
+    version='0.0.1',
+    packages = ['trm.utils',],
 
-      # metadata
-      author='Tom Marsh',
-      author_email='t.r.marsh@warwick.ac.uk',
+    # metadata
+    author='Tom Marsh',
+    author_email='t.r.marsh@warwick.ac.uk',
 
-      description="Python utility classes and functions",
-      long_description=long_description,
+    description="Python utility classes and functions",
+    long_description=long_description,
 
-      url='http://www.astro.warwick.ac.uk/',
+    url='http://www.astro.warwick.ac.uk/',
 
-      # Choose your license
-      license='BSD',
+    # Choose your license
+    license='BSD',
 
-      classifiers=[
-          # How mature is this project? Common values are
-          #   3 - Alpha
-          #   4 - Beta
-          #   5 - Production/Stable
-          'Development Status :: 3 - Alpha',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-          # Indicate who your project is intended for
-          'Intended Audience :: Astronomers',
-          'Topic :: Astronomy :: Photometric reduction',
+        # Indicate who your project is intended for
+        'Intended Audience :: Astronomers',
+        #'Topic :: Astronomy :: Photometric reduction',
 
-          # Pick your license as you wish (should match "license" above)
-          'License :: OSI Approved :: BSD License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
 
-          # Specify the Python versions you support here. In particular, ensure
-          # that you indicate whether you support Python 2, Python 3 or both.
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
-      ],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
 
-      # You can just specify the packages manually here if your project is
-      # simple. Or you can use find_packages().
-      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['astropy','matplotlib','numpy'],
 
-      install_requires=[
-          'astropy', 'matplotlib', 'numpy'
-      ],
-
-      # Makes significant use of f-strings which came in python v3.6
-      python_requires='>=3.6',
+    # Makes significant use of f-strings which came in python v3.6
+    python_requires='>=3.6',
 
   )
